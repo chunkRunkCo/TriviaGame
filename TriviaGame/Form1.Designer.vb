c007347c06fc58f99ddae4b176fcd924c08fc5aa
CCLC <cclc@lr.k12.nj.us>
2017-08-04 10:25:23 -0400

    TriviaGame

---
+++ TriviaGame/Form1.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(56, 21)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(56, 44)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(56, 67)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(57, 90)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.RadioButton3)
+        Me.Panel1.Controls.Add(Me.RadioButton4)
+        Me.Panel1.Controls.Add(Me.RadioButton1)
+        Me.Panel1.Controls.Add(Me.RadioButton2)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Location = New System.Drawing.Point(644, 172)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(203, 209)
+        Me.Panel1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(722, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 6
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(40, 62)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 31)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Question 2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(40, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 31)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Question 1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(56, 136)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(90, 31)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Submit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(137, 142)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(90, 31)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Question 3"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(137, 179)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(90, 31)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Question 4"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(137, 216)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(90, 31)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Question 5"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(112, 262)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(140, 77)
+        Me.Button7.TabIndex = 13
+        Me.Button7.Text = "FINAL QUESTION"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(1, -1)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(10, 10)
+        Me.Button8.TabIndex = 14
+        Me.Button8.Text = "Question 5"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(1033, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 15
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(641, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "score"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Location = New System.Drawing.Point(97, 43)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(170, 316)
+        Me.Panel2.TabIndex = 10
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(874, 386)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(52, 24)
+        Me.Button9.TabIndex = 17
+        Me.Button9.Text = "sup"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(926, 409)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(942, 448)
+        Me.MinimumSize = New System.Drawing.Size(942, 448)
+        Me.Name = "Form1"
+        Me.Text = "Logic Quiz"
+        Me.TransparencyKey = System.Drawing.Color.White
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button9 As Button
+End Class
---
+++ TriviaGame/Form1.vb
@@ -0,0 +1,126 @@
+﻿Public Class Form1
+    Dim question As Integer
+    Dim cheats As String
+    Dim score As Integer
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Panel1.Enabled = True
+        Label1.Text = "Why?"
+        'Panel2.Enabled = True
+        RadioButton1.Text = ("Because.")
+        RadioButton2.Text = ("Not my problem.")
+        RadioButton3.Text = ("Why not?")
+        RadioButton4.Text = ("Go away!")
+        question = 1
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If RadioButton2.Checked And question = 1 Then
+            MsgBox("Correct.")
+            score = score + 1
+        ElseIf RadioButton1.Checked And question = 2 Then
+            MsgBox("Correct.")
+            score = score + 2
+        ElseIf RadioButton4.Checked And question = 3 Then
+            MsgBox("Correct.")
+            score = score + 3
+        ElseIf RadioButton1.Checked And question = 4 Then
+            MsgBox("Correct.")
+            score = score + 4
+        ElseIf RadioButton3.Checked And question = 5 Then
+            MsgBox("Correct.")
+            score = score + 5
+        ElseIf RadioButton4.Checked And question = 6 Then
+            MsgBox("Correct.")
+            score = score + 6
+        Else
+            MsgBox("Incorrect.")
+        End If
+        Button2.Enabled = True
+
+        If Label2.Text = "Cheats Enabled" Then
+            score = score + 999999
+        End If
+        Panel1.Enabled = False
+        Label3.Text = score
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Panel1.Enabled = True
+        Label1.Text = "What is the answer?"
+        RadioButton1.Text = ("42")
+        RadioButton2.Text = ("24")
+        RadioButton3.Text = ("Nothing")
+        RadioButton4.Text = ("What solves the problem")
+        question = 2
+        Button2.Enabled = False
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Panel1.Enabled = True
+        Label1.Text = "What is the squared root of 144?"
+        RadioButton1.Text = ("Uh, 42?")
+        RadioButton2.Text = ("No.")
+        RadioButton3.Text = ("wut")
+        RadioButton4.Text = ("Duh, 12.")
+        question = 3
+        Button4.Enabled = False
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Panel1.Enabled = True
+        Label1.Text = "What time is it?"
+        RadioButton1.Text = ("I don't know.")
+        RadioButton2.Text = ("It is time.")
+        RadioButton3.Text = ("42 AM?")
+        RadioButton4.Text = ("Answer.")
+        question = 4
+        Button5.Enabled = False
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Panel1.Enabled = True
+        Label1.Text = "Say the alphabet backwards."
+        RadioButton1.Text = ("No thanks.")
+        RadioButton2.Text = ("zyxwvutsrq...")
+        RadioButton3.Text = ("tebahpla")
+        RadioButton4.Text = ("FFFFFFFFFF")
+        question = 5
+        Button6.Enabled = False
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Panel1.Enabled = True
+        Label1.Text = "What vegetable goes great in an undersea salad?"
+        RadioButton1.Text = ("Triangle.")
+        RadioButton2.Text = ("14?")
+        RadioButton3.Text = ("Teal.")
+        RadioButton4.Text = ("SEACUCUMBER!!!")
+        question = 6
+        Button7.Enabled = False
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Label2.Text = "Cheats Enabled"
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        MsgBox("B A Z I N G A")
+    End Sub
+End Class
